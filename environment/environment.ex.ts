f1079a11fc18b1181c343470bc150dbe70d5eb27
SDBagel <sdbagel@gmail.com>
2021-06-12 19:12:59 -0700

    repo: Copy server from monorepo

---
+++ environment/environment.ex.ts
@@ -0,0 +1,11 @@
+
+export const config = {
+  // Serve Port
+  port: 8002,
+  // DB settings
+  database_name: "dev",
+  // Github ClientID, Secret, Callback
+  client_id: "",
+  client_secret: "", 
+  redirect_uri: "https://maintained.cc/oauth/callback"
+}
---
+++ services/mod.ts
@@ -0,0 +1,4 @@
+
+export * from './auth/auth.ts';
+export * from './badge/badge.ts';
+export * from './data.ts';

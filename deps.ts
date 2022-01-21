a20dc2f72b96fb69a02262ec5be95e6140da619f
SDBagel <sdbagel@gmail.com>
2022-01-20 21:19:46 -0800

    repo: Update dependencies

--- deps.ts
+++ deps.ts
@@ -2,12 +2,12 @@
 import "https://deno.land/x/dotenv@v3.1.0/load.ts";
 
 // Oak (Server)
-export { Application, Router, send } from "https://deno.land/x/oak@v7.6.2/mod.ts";
+export { Application, Router, send } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 
 // Service dependencies
 export { exists } from "https://deno.land/std@0.99.0/fs/exists.ts";
-export { crypto } from "https://deno.land/std@0.108.0/crypto/mod.ts";
-export { jwtVerify } from "https://deno.land/x/jose@v3.18.0/jwt/verify.ts";
+export { crypto } from "https://deno.land/std@0.122.0/crypto/mod.ts";
+export { jwtVerify } from "https://deno.land/x/jose@v4.3.8/jwt/verify.ts";
 
 // Mongo (Database)
-export { MongoClient, Collection, Database } from "https://deno.land/x/mongo@v0.27.0/mod.ts";
+export { MongoClient, Collection, Database } from "https://deno.land/x/mongo@v0.29.0/mod.ts";

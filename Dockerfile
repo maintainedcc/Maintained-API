af8ea0e7883400297aab7139095cd22bc6023ceb
SDBagel <sdbagel@gmail.com>
2022-02-12 14:20:51 -0800

    repo: Update dependencies, Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM denoland/deno:alpine-1.11.1
+FROM denoland/deno:alpine-1.18.2
 
 EXPOSE 8000
 EXPOSE 8001
@@ -9,7 +9,7 @@ WORKDIR /app
 USER deno
 
 COPY . .
-RUN deno cache --unstable deps.ts
-RUN deno cache --unstable main.ts
+RUN deno cache deps.ts
+RUN deno cache main.ts
 
-CMD ["run", "--allow-net", "--allow-read", "--allow-write", "--allow-plugin", "--unstable", "main.ts"]
+CMD ["run", "--allow-net", "--allow-read", "--allow-write", "--allow-env", "main.ts"]
--- deps.ts
+++ deps.ts
@@ -5,9 +5,8 @@ import "https://deno.land/x/dotenv@v3.1.0/load.ts";
 export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 
 // Service dependencies
-export { exists } from "https://deno.land/std@0.99.0/fs/exists.ts";
 export { crypto } from "https://deno.land/std@0.122.0/crypto/mod.ts";
 export { jwtVerify } from "https://deno.land/x/jose@v4.3.8/jwt/verify.ts";
 
 // Mongo (Database)
-export { MongoClient, Collection, Database } from "https://deno.land/x/mongo@v0.29.0/mod.ts";
+export { MongoClient, Collection, Database } from "https://deno.land/x/mongo@v0.29.1/mod.ts";

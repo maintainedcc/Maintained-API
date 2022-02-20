d4b93f1674edd9529d0897216cfdd7c23a26a033
SDBagel <sdbagel@gmail.com>
2022-02-20 14:00:55 -0800

    refactor: Use env variable for HMAC key

--- services/auth/auth.ts
+++ services/auth/auth.ts
@@ -1,12 +1,6 @@
 
 import { jwtVerify } from "../../deps.ts";
-import { secret } from "./private.key.ts";
-
-const key = await crypto.subtle.importKey(
-	"raw", secret,
-	{ name: "HMAC", hash: "SHA-256" } as any,
-	false, ["verify"]
-) as CryptoKey;
+import { key } from "./key.ts";
 
 export class AuthService {
 
